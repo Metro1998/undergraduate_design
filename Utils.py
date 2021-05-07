f8077cb749fbb098b2834665c53418392bac0f5f
Metro1998 <3170101740@zju.edu.cn>
2021-05-07 14:17:17 +0800

    please

--- Utils.py
+++ Utils.py
@@ -44,27 +44,6 @@ def get_veh_position_type(vehID_list):
         res.append(vehicles_direction)
     return res
 
-"""
-def get_veh_type(vehID_list):
-    vehicle = []
-    tem = []
-    vehicles_direction = []
-    res = []
-    for i in vehID_list:
-        for j in i:
-            traci.vehicle.subscribe(str(j), (tc.VAR_TYPE,))
-            for k in traci.vehicle.getSubscriptionResults(str(j)).values():
-                # Only one loop, but transform tuple to the list
-                vehicle = list(k)
-                print(vehicle)
-                if len(vehicle) == 14:
-                    tem = [0]
-                if len(vehicle) == 15:
-                    tem = [2]
-                else:
-                    tem = [1]
-            vehicles_direction.append(tem)
-        res.append(vehicles_direction)
-    return res
+def chessboard(vehicle_position_type):
 
-"""
+
